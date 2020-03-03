cbea7829d25fee7e53a870cc1e2629590b754283
João Alves <joaoalves.cs@gmail.com>
2020-03-03 18:37:17 -0300

    pouca coisa para acabar a primeira fase!

--- levels/backdoor.gd
+++ levels/backdoor.gd
@@ -6,7 +6,7 @@ var posicao_final
 var velocidade = 0.6
 
 func _ready():
-	$Sprite.play("walk")
+	$Sprite.play("move")
 	posicao_inicial = $".".position.x
 	posicao_final = posicao_inicial + 100
 
@@ -14,13 +14,13 @@ func _ready():
 func _process(_delta):
 	if(posicao_inicial <= posicao_final and flip):
 		$".".position.x += velocidade
-		$Sprite.flip_h = false
+		#$Sprite.flip_h = false
 		if ($".".position.x >= posicao_final):
 			flip = false
 	
 	if($".".position.x >= posicao_inicial and !flip):
 		$".".position.x -= velocidade
-		$Sprite.flip_h = true
+		#$Sprite.flip_h = true
 		if ($".".position.x <= posicao_inicial):
 			flip = true
 			
